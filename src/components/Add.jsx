7a922177991b7228d3a413455d2b506b92e0096c
Najas06 <najasneju@gmail.com>
2023-12-27 23:41:41 +0530

    started-AddSection

---
+++ src/components/Add.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Add() {
+  return (
+    <>
+    <div className='d-flex align-items-center'>
+        <h5>Upload New Video</h5>
+        <button className='btn'><i class="fa-solid fa-cloud-arrow-up fs-5 " style={{color:"white"}}></i></button>
+    </div>
+    </>
+    )
+}
+
+export default Add
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Add from '../components/Add'
+import Category from '../components/Category'
+import View from '../components/View'
+import Videocard from '../components/Videocard'
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+    {/* <Add/>
+    <Category/>
+    <View/>
+    <Videocard/> */}
+    <div className="container mt-5 mb-5 d-flex justify-content-between align-item-center">
+      <div className='add_videos'>
+        <Add/>
+      </div>
+      <div>
+        Watch Histrory
+      </div>
+    </div>
+    </>    
   )
 }
 
